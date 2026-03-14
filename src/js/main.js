cff31fc08693e0fabd94ba50c42bfa2281dc659b
Henry Chizoba <henrychizobaudeh@gmail.com>
2026-03-14 06:50:15 +0100

    feat: implement product list rendering and update main script (#9)

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+import { qs } from './utils.mjs';
+
+const dataSource = new ProductData('tents');
+const productList = new ProductList('tents', dataSource, qs('.product-list'));
+
+productList.init();
