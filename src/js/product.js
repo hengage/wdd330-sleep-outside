6af959397670d67f864d46ae85bdeeb7d5b217d9
Henry Chizoba <henrychizobaudeh@gmail.com>
2026-03-14 05:22:53 +0100

    Revert "Hcu team2/dynamic product details (#6)"
    This reverts commit 758bc7ce4a33a2dad20dfd3fd1674a663949b0e3.

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,29 @@
-import { getParam } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
 
-const productId = getParam('product');
 const dataSource = new ProductData('tents');
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+function addProductToCart(product) {
+  const currentCart = getLocalStorage('so-cart') || [];
+  const existingProductIndex = currentCart.findIndex(
+    (item) => item.Id === product.Id,
+  );
+
+  if (existingProductIndex !== -1) {
+    currentCart[existingProductIndex].qty++;
+  } else {
+    product.qty = 1;
+    currentCart.push(product);
+  }
+  setLocalStorage('so-cart', currentCart);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,6 +2,8 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
+// or a more concise version if you are into that sort of thing:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
@@ -13,15 +15,9 @@ export function setLocalStorage(key, data) {
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener('touchend', (event) => {
+  qs(selector).addEventListener("touchend", (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener('click', callback);
-}
-// extract parameter from URL query string
-export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
+  qs(selector).addEventListener("click", callback);
 }
