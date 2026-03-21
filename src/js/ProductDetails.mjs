6acc1a2d2eb023adcefd2341b46a1065b3aabbac
Marcus <juandbeta@gmail.com>
2026-03-21 06:35:40 +0100

    w2 activity done

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,36 @@
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails(this.product)
+        document.getElementById('addToCart').addEventListener('click', this.addProductToCart.bind(this));
+    }
+
+    renderProductDetails(product) {
+        document.querySelector('h3').textContent = product.Brand.Name;
+        document.querySelector("h2").textContent = product.NameWithoutBrand;
+        document.getElementById("product_img").src = product.Image;
+        document.getElementById("product_img").alt = product.NameWithoutBrand;
+        document.getElementsByClassName("product-card__price")[0].textContent = `Price: $${product.ListPrice}`;
+        document.getElementsByClassName("product__color")[0].textContent = `Colors: ${product.Colors[0].ColorName}`;
+        document.getElementsByClassName("product__description")[0].innerHTML = product.DescriptionHtmlSimple;
+
+        document.getElementById('addToCart').dataset.productId = product.Id;
+    }
+
+    addProductToCart() {
+        const cart = getLocalStorage('cart') || [];
+        cart.push(this.product);
+        setLocalStorage('cart', cart);
+        console.log('Product added to cart:', this.product);
+    }
+}
+
--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,10 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { getParam, getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+
 
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage('so-cart') || [];
-  const existingProductIndex = currentCart.findIndex(
-    (item) => item.Id === product.Id,
-  );
-
-  if (existingProductIndex !== -1) {
-    currentCart[existingProductIndex].qty++;
-  } else {
-    product.qty = 1;
-    currentCart.push(product);
-  }
-  setLocalStorage('so-cart', currentCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
